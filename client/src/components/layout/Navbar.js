5c62feb6b0d603aa0365beda2d0cd844f6c3915f
KA <kareemalasmar@gmail.com>
2020-02-10 17:31:27 -0800

    redirect authenticated user from landing to dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
@@ -34,7 +40,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code' />
+          <i className='fas fa-code' /> DevConnector
         </Link>
       </h1>
       {!loading && (
